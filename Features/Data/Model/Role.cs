4284486eee7acbde3b75fb54635742e1edd52565
Ph4n70mC0de <arjscandes73@gmail.com>
2026-05-12 23:11:12 +0800

    add Role model

---
+++ Features/Data/Model/Role.cs
@@ -0,0 +1,13 @@
+namespace ProductManagement.Features.Data.Model
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
