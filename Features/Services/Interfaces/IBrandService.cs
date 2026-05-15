587afd55e4d53fc3381c7479448013b15e8f2c04
Ph4n70mC0de <arjscandes73@gmail.com>
2026-05-15 21:43:42 +0800

    Add pagination support to IBrandService
    Update IBrandService interface to include GetBrandsPagedAsync, enabling paginated retrieval of brand entities using PagedResult.

--- Features/Services/Interfaces/IBrandService.cs
+++ Features/Services/Interfaces/IBrandService.cs
@@ -1,3 +1,4 @@
+using ProductManagement.Features.Data;
 using ProductManagement.Features.Data.Model;
 
 namespace ProductManagement.Features.Services.Interfaces
@@ -5,6 +6,7 @@ namespace ProductManagement.Features.Services.Interfaces
     public interface IBrandService
     {
         Task<IEnumerable<Brand>> GetAllBrandsAsync();
+        Task<PagedResult<Brand>> GetBrandsPagedAsync(int pageNumber, int pageSize);
         Task<Brand?> GetBrandByIdAsync(int id);
         Task<Brand> CreateBrandAsync(Brand brand);
         Task<Brand> UpdateBrandAsync(Brand brand);
