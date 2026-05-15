bf81aa0312e64a56ee90c363417462ee8378709f
Ph4n70mC0de <arjscandes73@gmail.com>
2026-05-15 21:13:58 +0800

    Add GetPagedAsync to IGenericRepository
    Add support for paginated data retrieval by introducing the GetPagedAsync method to the generic repository interface.

--- Features/Repositories/Interfaces/IGenericRepository.cs
+++ Features/Repositories/Interfaces/IGenericRepository.cs
@@ -1,3 +1,4 @@
+using ProductManagement.Features.Data;
 using ProductManagement.Features.Data.Model;
 
 namespace ProductManagement.Features.Repositories.Interfaces
@@ -5,6 +6,7 @@ namespace ProductManagement.Features.Repositories.Interfaces
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<PagedResult<T>> GetPagedAsync(int pageNumber, int pageSize);
         Task<T?> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
